cbd97a2c0de6724cee71216ca333429052d53bed
Tomas Ossa <tomasossaefcsl@gmail.com>
2021-04-07 16:24:52 -0500

    methods addPerson , returnTypeId and return the penultimate number has been done in miniMarket

--- src/model/MiniMarket.java
+++ src/model/MiniMarket.java
@@ -17,7 +17,39 @@ public class MiniMarket {
 		peopleWhoEntered=new ArrayList<>();
 	}
 	
+	public void addPerson(IdType idType, String id, int day) throws MinorException, PicoCedulaException{
+		
+		if(idType==IdType.TI) {
+			throw new MinorException();
+		}
+		else {
+			if(returnPenultimateNumber(id)%2==0) {//si el penultimo digito es par
+				if(day%2==0) {
+					throw new PicoCedulaException();
+				}
+				else {// si el penultimo digito es par y el dia es impar
+					peopleWhoEntered.add(new Person(idType, id));
+				}
+			}
+			else {//si el penultimo digito es impar
+				if(day%2!=0) {
+					throw new PicoCedulaException();
+				}
+				else {// si el penultimo digito es impar y el dia es par
+					peopleWhoEntered.add(new Person(idType, id));
+				}
+			}
+		}
+		quantityOfPeople++;// Asi tenga TI o alguna otra
+	}
 	
+	public int returnPenultimateNumber(String cadena) {
+		int number=-1;
+		String[] parts = cadena.split("|");
+		number=Integer.parseInt(parts[parts.length-2]);
+		return number;
+		
+	}
 
 	public int getQuantityOfPeople() {
 		return quantityOfPeople;
@@ -35,6 +67,23 @@ public class MiniMarket {
 		this.peopleWhoEntered = peopleWhoEntered;
 	}
 	
+	public IdType returnIdType(String type) {
+		IdType idType=null;
+		if(type.equals("1")) {
+			idType=IdType.CC;
+		}
+		else if(type.equals("2")) {
+			idType=IdType.TI;
+		}
+		else if(type.equals("3")) {
+			idType=IdType.PP;
+		}
+		else if(type.equals("4")) {
+			idType=IdType.CE;
+		}
+		
+		return idType;
+	}
 	
 	
 }
